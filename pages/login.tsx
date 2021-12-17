6426aadf37bb1f3874311ff3cb10eb59fd51d1e2
hvlopez <hectorvmlopez@gmail.com>
2021-12-17 11:09:20 -0500

    setting next-auth

---
+++ pages/login.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import SideBar from "../components/SideBar/Sidebar";
+
+const Login: NextPage = () => {
+  return (
+    <div className="">
+      <h1>This is login page</h1>
+    </div>
+  );
+};
+
+export default Login;
